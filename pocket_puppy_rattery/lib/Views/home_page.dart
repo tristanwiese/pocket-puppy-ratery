d90ee12288a116733e81e242cef6baceae44c66d
tristanwiese <tristanwiese7472@gmail.com>
2023-04-16 14:39:11 +0200

    Added database

--- pocket_puppy_rattery/lib/Views/home_page.dart
+++ pocket_puppy_rattery/lib/Views/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -10,44 +11,59 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      floatingActionButtonLocation: FloatingActionButtonLocation.miniCenterFloat,
-      appBar: myAppBar(context),
-      floatingActionButton: myFloatingActionButton(),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: const[BottomNavigationBarItem(icon: Icon(Icons.details_outlined), label: 'Details'), BottomNavigationBarItem(icon: Icon(Icons.logout_outlined), label: 'Logout')]),
-      body: Center(child: myBody()),
-    );
+    return Scaffold(
+        floatingActionButtonLocation:
+            FloatingActionButtonLocation.miniCenterFloat,
+        appBar: myAppBar(context),
+        floatingActionButton: myFloatingActionButton(),
+        bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.line_style_outlined), label: 'Details'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.logout_outlined), label: 'Logout')
+            ]),
+        body: StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance.collection('rats').snapshots(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasData) {
+                final rats = snapshot.data!.docs;
+                print(rats);
+                return Center(child: myBody(rats));
+              }
+
+              return const Center(
+                  child: Text(
+                      'Oh no! I guess we will have to get you some rats!'));
+            }));
   }
 
-  myBody() => ConstrainedBox(
-    constraints: BoxConstraints(maxWidth:  MediaQuery.of(context).size.width / 3),
-    child: ListView.builder(
-      itemCount: 10,
-      itemBuilder: (BuildContext context, i) {
-      return Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListTile(
-          title: Text('Rat ${i + 1}'),
-          shape: const BeveledRectangleBorder(side: BorderSide(width: 1), borderRadius: BorderRadius.all(Radius.circular(15))),
-          contentPadding: const EdgeInsets.all(10),
-        ),
+  myBody(rats) => ConstrainedBox(
+        constraints:
+            BoxConstraints(maxWidth: MediaQuery.of(context).size.width / 3),
+        child: ListView.builder(
+            itemCount: rats.length,
+            itemBuilder: (BuildContext context, i) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  title: Text(rats[i]['name']),
+                  shape: const BeveledRectangleBorder(
+                      side: BorderSide(width: 1),
+                      borderRadius: BorderRadius.all(Radius.circular(15))),
+                  contentPadding: const EdgeInsets.all(10),
+                ),
+              );
+            }),
       );
-      }
-      ),
-  );
 
   FloatingActionButton myFloatingActionButton() => FloatingActionButton(
-    onPressed: (){
-
-    },
-    tooltip: "Add Rat",
-    child: const Icon(Icons.add),
-    );
-  myAppBar (BuildContext context){
-    return AppBar(
-
-    );
+        onPressed: () {},
+        tooltip: "Add Rat",
+        child: const Icon(Icons.add),
+      );
+  myAppBar(BuildContext context) {
+    return AppBar();
   }
 }
--- pocket_puppy_rattery/lib/main.dart
+++ pocket_puppy_rattery/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-
 import 'Views/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
